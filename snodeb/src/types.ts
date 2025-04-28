040b0e36a96b457313190d2eee5602749ee5602d
Sonel Prabhakaran <sonel.prabhakaran@sageautomation.com>
2025-04-28 10:39:14 +0930

    fix: make all options optional

--- snodeb/src/types.ts
+++ snodeb/src/types.ts
@@ -3,29 +3,29 @@ import path from "node:path";
 export const templateDir = path.join(__dirname, "../public/templates");
 
 export interface BuildConfig {
-  name: string;
-  version: string;
-  description: string;
-  maintainer: string;
-  architecture: string;
-  depends: string[];
+  name?: string;
+  version?: string;
+  description?: string;
+  maintainer?: string;
+  architecture?: string;
+  depends?: string[];
   extends?: string;
-  systemd: {
-    user: string;
-    group: string;
-    entryPoint: string;
-    restart: "always" | "on-failure" | "no";
-    restartSec: number;
-    enableService: boolean;
-    startService: boolean;
-    useNodeExecutor: boolean;
+  systemd?: {
+    user?: string;
+    group?: string;
+    entryPoint?: string;
+    restart?: "always" | "on-failure" | "no";
+    restartSec?: number;
+    enableService?: boolean;
+    startService?: boolean;
+    useNodeExecutor?: boolean;
   };
-  files: {
-    include: string[];
-    exclude: string[];
-    installPath: string;
-    configInclude: string[];
-    configExclude: string[];
+  files?: {
+    include?: string[];
+    exclude?: string[];
+    installPath?: string;
+    configInclude?: string[];
+    configExclude?: string[];
   };
   customScripts?: {
     preinst?: string;
